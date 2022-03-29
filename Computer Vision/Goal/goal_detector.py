27b6722735dcf3e8c9a545b859306ee6f2d12974
abbwiljoh <william.johansson@abbindustrigymnasium.se>
2022-03-29 16:23:45 +0200

    fortsättning goal detector (nästan klar)

--- Computer Vision/Goal/goal_detector.py
+++ Computer Vision/Goal/goal_detector.py
@@ -4,12 +4,11 @@
 """
 
 import cv2
-from cv2 import cvtColor
 import matplotlib.pyplot as plt
 import numpy as np
 
 for f in range(1, 2):
-    default_image = f'Goal\img\goal{f}.png'
+    default_image = f'Computer Vision\Goal\img\goal{f}.png'
 
     img = cv2.imread(default_image, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (1080, 720), interpolation=cv2.INTER_AREA)
@@ -21,33 +20,37 @@ for f in range(1, 2):
 
     blank = np.zeros((height, width, 3), dtype=np.uint8)
 
-    ret, thresh = cv2.threshold(img, 127, 255, 0)
-    cv2.imshow("bw", thresh)
-    cv2.waitKey(0)
+    ret, thresh = cv2.threshold(img, 150, 255, 0)
     contours, hierarchy = cv2.findContours(
         thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    sortedContours = sorted(
-        contours, key=lambda x: cv2.contourArea(x), reverse=True)[:25]
-
     # cv2.drawContours(blank, sortedContours, -1, (0, 0, 255), 3)
-
+    newContours = []
     # Pattern matching (5 vita rutor i höjdled, 7 stycken i sidled)
+    # Rutor höjd/bredd ungefär lika med 25.5/9.5 = 2.7 +/- 0.3
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        if 2.1 <= h/w <= 2.9 or 0.29 <= w/h <= 0.45:
+            newContours.append(cnt)
+            # cv2.rectangle(blank, (x, y), (x+w, y+h), (255, 255, 255), 2)
+        # rect = cv2.minAreaRect(cnt)
+        # box = cv2.boxPoints(rect)
+        # box = np.int0(box)
+        # cv2.drawContours(blank, [box], 0, (255, 0, 255), 2)
+
+    sortedContours = sorted(
+        newContours, key=lambda x: cv2.contourArea(x), reverse=True)[:17]
+
     for cnt in sortedContours:
-        rect = cv2.minAreaRect(cnt)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        cv2.drawContours(blank, [box], 0, (0, 0, 255), 2)
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(blank, (x, y), (x+w, y+h), (255, 255, 255), 2)
 
     # Edge detection
     dst = cv2.Canny(blank, 100, 200, None, 3)
     cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
 
     # # Using probabilistic Hough Line Transform to find lines:
-    lines = cv2.HoughLinesP(dst, 1, np.pi/180, 50, None, 100, 10)
-
-    # # Regular Hough Line transform
-    # lines = cv2.HoughLines(dst, 1, np.pi/180, 105, None, 0, 0)
+    # lines = cv2.HoughLinesP(dst, 1, np.pi/180, 50, None, 100, 10)
 
     # if lines is not None:
     #     for i in range(0, len(lines)):
@@ -62,16 +65,19 @@ for f in range(1, 2):
 
     #         cv2.line(cdst, pt1, pt2, (0, 255, 255), 1, cv2.LINE_AA)
 
-    if lines is not None:
-        for i in range(0, len(lines)):
-            l = lines[i][0]
-            cv2.line(cdst, (l[0], l[1]), (l[2], l[3]),
-                     (0, 255, 255), 2, cv2.LINE_AA)
+    # # Regular Hough Line transform
+    # lines = cv2.HoughLines(dst, 1, np.pi/180, 105, None, 0, 0)
 
-    cv2.imshow("Detected Lines (yellow)", cdst)
+    # if lines is not None:
+    #     for i in range(0, len(lines)):
+    #         l = lines[i][0]
+    #         cv2.line(cdst, (l[0], l[1]), (l[2], l[3]),
+    #                  (0, 255, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow("output", cdst)
     cv2.waitKey()
 
-    print(lines)
+    # print(lines)
 
     # !!!
     # FIXA SPAGhETTI
