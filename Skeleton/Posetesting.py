0af0f89570b8b640ba6982e3c8d180cee88cb606
HarleyBerglund <55533374+HarleyBerglund@users.noreply.github.com>
2022-03-30 09:16:10 +0200

    fpaboeuofbeåuonvpi

--- Skeleton/Posetesting.py
+++ Skeleton/Posetesting.py
@@ -8,25 +8,24 @@ pTime = 0
 detector = pm.poseDetector()
 
 
-def calcFPS(img, pTime):
-    cTime = time.time()
-    fps = 1/(cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (70, 50),
-                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-    return img
 
 def captureVideo():
+    cap = cv2.VideoCapture(0)
+    pTime = 0
     while True:
         success, img = cap.read()
         img = detector.findPose(img)
         lmList = detector.findPosition(img)
-        # print(lmList[14])    //Specific Datapoint
-        print(lmList)  # All points
+        print(lmList)
 
-        img = calcFPS(img, pTime)
+        cTime = time.time()
+        fps = 1/(cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (70, 50),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
         cv2.imshow("image", img)
         cv2.waitKey(1)
 
-    cv2.waitKey(1)
 captureVideo()
