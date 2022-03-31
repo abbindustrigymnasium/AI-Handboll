23c65dd849ec793e3fcd2c52501fa2d9bea565a1
AbbMyBjo <54663051+AbbMyBjo@users.noreply.github.com>
2022-03-31 16:17:39 +0200

    hemsida

---
+++ AI-handboll/constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = '#000';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#fff',
+    background: '#FFE599',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#000',
+    background: '#FFE599',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
