27ab54c45cfed83804a04ff76be195654dc08446
abbwiljoh <william.johansson@abbindustrigymnasium.se>
2022-03-31 16:11:18 +0200

    Början till pattern matching (wip, ingen effekt)

--- Computer Vision/Goal/goal_detector.py
+++ Computer Vision/Goal/goal_detector.py
@@ -7,6 +7,17 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def sortBoxPoints(boxPoints: list) -> list:
+    """
+    Returns list with coordinates sorted from top left to bottom left (tl,tr,br,bl) of boxPoint list.
+    """
+    y_sorted = sorted(boxPoints, key=lambda point: point[1], reverse=True)
+    top_right_to_left = sorted(y_sorted[:2], key=lambda p: p[0], reverse=True)
+    bottom_left_to_right = sorted(y_sorted[2:], key=lambda p: p[0])
+    return bottom_left_to_right + top_right_to_left
+
+
 for f in range(1, 2):
     default_image = f'Computer Vision\Goal\img\goal{f}.png'
 
@@ -19,35 +30,58 @@ for f in range(1, 2):
     width = shape[1]
 
     blank = np.zeros((height, width, 3), dtype=np.uint8)
+    bcop = blank
 
     ret, thresh = cv2.threshold(img, 150, 255, 0)
     contours, hierarchy = cv2.findContours(
         thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # cv2.drawContours(blank, sortedContours, -1, (0, 0, 255), 3)
     newContours = []
+
     # Pattern matching (5 vita rutor i höjdled, 7 stycken i sidled)
     # Rutor höjd/bredd ungefär lika med 25.5/9.5 = 2.7 +/- 0.3
-    for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        if 2.1 <= h/w <= 2.9 or 0.29 <= w/h <= 0.45:
-            newContours.append(cnt)
-            # cv2.rectangle(blank, (x, y), (x+w, y+h), (255, 255, 255), 2)
-        # rect = cv2.minAreaRect(cnt)
-        # box = cv2.boxPoints(rect)
-        # box = np.int0(box)
-        # cv2.drawContours(blank, [box], 0, (255, 0, 255), 2)
 
-    sortedContours = sorted(
-        newContours, key=lambda x: cv2.contourArea(x), reverse=True)[:17]
+    alignment_x = []
+    alignment_boxes = []
 
-    for cnt in sortedContours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        cv2.rectangle(blank, (x, y), (x+w, y+h), (255, 255, 255), 2)
+    for cnt in contours[2:]:
+        rect = cv2.minAreaRect(cnt)
+        box = cv2.boxPoints(rect)
+
+        points = sortBoxPoints(box)
+        midPoint = round((points[0][0]+points[1][0])/2)
+        midPoint_y = round((points[2][1] + points[0][1])/2)
+
+        dist_x = abs(points[1][0]-points[0][0])
+        dist_y = abs(points[1][1]-points[2][1])
+        area = dist_x*dist_y
+
+        box = np.int0(box)
+        cv2.drawContours(bcop, [box], 0, (255, 255, 255), 2)
+
+        idx = -1
+        for a in alignment_x:
+            if a[0]-0.2*a[1] <= midPoint <= a[0]+0.2*a[1]:
+                # if a[2] * 0.5 <= area <= a[2] * 2 or True:
+                idx = alignment_x.index(a)
+                alignment_boxes[idx].append(box)
+        if idx == -1:
+            alignment_x.append([midPoint, dist_x, area])
+            alignment_boxes.append([box])
+
+    filter(lambda x: len(x) <= 7, alignment_boxes)
+    boxes = sorted(alignment_boxes, key=lambda l: len(l), reverse=True)
+
+    for als in boxes:
+        for b in als:
+            cv2.drawContours(blank, [b], 0, (255, 255, 255), 2)
+    # if 2.1 <= h/w <= 2.9 or 0.29 <= w/h <= 0.45:
+    # cv2.rectangle(blank, (x, y), (x+w, y+h), (255, 255, 255), 2)
 
     # Edge detection
     dst = cv2.Canny(blank, 100, 200, None, 3)
     cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    cblank = cv2.Canny(bcop, 100, 200, None, 3)
+    ccblank = cv2.cvtColor(cblank, cv2.COLOR_GRAY2BGR)
 
     # # Using probabilistic Hough Line Transform to find lines:
     # lines = cv2.HoughLinesP(dst, 1, np.pi/180, 50, None, 100, 10)
@@ -73,13 +107,10 @@ for f in range(1, 2):
     #         l = lines[i][0]
     #         cv2.line(cdst, (l[0], l[1]), (l[2], l[3]),
     #                  (0, 255, 255), 2, cv2.LINE_AA)
-
-    cv2.imshow("output", cdst)
+    cv2.imshow("Original", ccblank)
+    cv2.imshow("\"Pattern Detection\"", cdst)
     cv2.waitKey()
 
     # print(lines)
 
-    # !!!
-    # FIXA SPAGhETTI
-    # Pattern matching/detection
-    # !!!
+# Gör så att pattern matching faktiskt gör nånting!!
