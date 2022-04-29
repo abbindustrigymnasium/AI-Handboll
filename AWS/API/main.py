6ae09448f5093b6676a13d1d7bf6e86837b5f69d
abbjondam <jonathan.damsgaardfalck@abbindustrigymnasium.se>
2022-04-29 10:48:57 +0200

    update api

--- AWS/API/main.py
+++ AWS/API/main.py
@@ -1,14 +1,30 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, UploadFile, File
 from presignedURL import presignedURL
+from feedback import feedback
+
 
 app = FastAPI()
 
 @app.get("/get/")
 def signedURL(filename: str):
-    return presignedURL(filename= filename, mode= 'get_object')
+    return presignedURL(filename=filename, mode='get_object')
 
 
+@app.get("/feedback/")
+def feedback(filename: str):
+    return feedback(filename=filename)
+    
 
-@app.get("/put/")
-def signedURL(filename: str):
-    return  presignedURL(filename= filename, mode= 'put_object')
+@app.post("/uploadfile")
+async def create_upload_file(data: UploadFile = File(...)):
+    print(data.filename)
+
+    file_location = f"files/{data.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(data.file.read())
+
+    os.system(f'sudo aws s3 cp {file_location} s3://handboll-ai-coach/videos/{data.filename}')
+    os.system(f'rm {file_location}')
+
+    return {"Filename": data.filename}
