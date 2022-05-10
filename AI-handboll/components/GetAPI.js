e4fa4f99e87f932b124a025d488624cd2d6d9f16
Lucas <Lucas.venlock@abbindustrigymnasium.se>
2022-05-10 09:05:58 +0200

    Create GetAPI.js

---
+++ AI-handboll/components/GetAPI.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Box, FlatList, Center, NativeBaseProvider, Button } from "native-base";
+
+export default function CoffeeClick() {
+  const [data, setData] = useState(null);
+  const [visible, setVisible] = useState(true);
+
+  const fetchData = async () => {
+    const resp = await fetch("https://api.sampleapis.com/coffee/hot");
+    const data = await resp.json();
+    setData(data);
+    setVisible(false);
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <Box px={5} py={2} rounded="md" bg="primary.300" my={2}>
+        {item.title}
+      </Box>
+    );
+  };
+}
